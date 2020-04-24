b79f32460763d7f5012ae04fa48b6bf7ce193d69
Cristian Verdes <v-crverd@microsoft.com>
2020-04-24 20:09:57 +0300

    Refactor SurfaceDuoLayout with the customers feedback
    Modifications:
    	- Removed SurfaceDuoScreenManager and ScreenModeListener
    	- SurfaceDuoLayout has no longer dependecies to other classes that need initialisation.
    	- Make views inside SurfaceDuoLayout available in onCreate()
    	- Handle configuration changes in SurfaceDuoLayout
    	- Improve performance

--- sample-dualscreen-layout/src/main/java/com/microsoft/device/surfaceduo/displaysample/MainActivity.kt
+++ sample-dualscreen-layout/src/main/java/com/microsoft/device/surfaceduo/displaysample/MainActivity.kt
@@ -1,8 +1,12 @@
+/*
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License.
+ */
+
 package com.microsoft.device.surfaceduo.displaysample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.microsoft.device.dualscreen.layout.ScreenModeListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
