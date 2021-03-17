a1d31b3c23688bd0356ca5e6e977263359b27866
Craig Dunn <craig.dunn@microsoft.com>
2021-03-17 15:05:49 -0700

    add rainbow ink and highlighter

--- inksdk/inksample/src/main/java/com/microsoft/device/inksample/MainActivity.kt
+++ inksdk/inksample/src/main/java/com/microsoft/device/inksample/MainActivity.kt
@@ -71,12 +71,17 @@ class MainActivity : AppCompatActivity() {
     fun fancySwitchChanged(view: View) {
         var switch = view as Switch
         if (switch.isChecked) {
-            inkView.dynamicPaintHandler = FancyPaintHandler()
+            //inkView.dynamicPaintHandler = FancyPaintHandler()
+            inkView.dynamicPaintHandler = HighlighterPaintHandler()
+            //inkView.dynamicPaintHandler = RainbowPaintHandler()
         } else {
             inkView.dynamicPaintHandler = null
         }
     }
 
+    /**
+     * Renders the ink with transparency linked to the pressure on the pen.
+     */
     inner class FancyPaintHandler : DynamicPaintHandler {
         override fun generatePaintFromPenInfo(penInfo: InputManager.PenInfo): Paint {
             var paint = Paint()
@@ -94,6 +99,69 @@ class MainActivity : AppCompatActivity() {
                 TypedValue.COMPLEX_UNIT_DIP,
                 penInfo.pressure * 10 + 5,
                 resources.displayMetrics
+            )
+            paint.style = Paint.Style.STROKE
+            paint.strokeJoin = Paint.Join.ROUND
+            paint.strokeCap = Paint.Cap.ROUND
+
+            return paint
+        }
+    }
+
+    /**
+     * Renders the ink as though from a highlighter: permanently transparent
+     * and yellow-colored.
+     */
+    inner class HighlighterPaintHandler : DynamicPaintHandler {
+        override fun generatePaintFromPenInfo(penInfo: InputManager.PenInfo): Paint {
+            var paint = Paint()
+            val alpha = 80
+
+            paint.color = Color.argb(
+                    alpha.toInt(),
+                    Color.YELLOW.red,
+                    Color.YELLOW.green,
+                    Color.YELLOW.blue
+            )
+            paint.isAntiAlias = true
+            // Set stroke width based on display density.
+            paint.strokeWidth = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    penInfo.pressure * 10 + 5,
+                    resources.displayMetrics
+            )
+            paint.style = Paint.Style.STROKE
+            paint.strokeJoin = Paint.Join.BEVEL
+            paint.strokeCap = Paint.Cap.BUTT
+
+            return paint
+        }
+    }
+
+    /**
+     * Renders the ink as a continually changing color line
+     * with a rainbow effect (similar to Microsoft Whiteboard)
+     */
+    inner class RainbowPaintHandler : DynamicPaintHandler {
+
+        var frequency = .3
+        var i = 0 // TODO: fix overflow
+
+        override fun generatePaintFromPenInfo(penInfo: InputManager.PenInfo): Paint {
+            var paint = Paint()
+            i++
+            paint.color = Color.argb(
+                    255,
+                    (Math.sin(frequency*i + 0) * 127 + 128).toInt(),
+                    (Math.sin(frequency*i + 2) * 127 + 128).toInt(),
+                    (Math.sin(frequency*i + 4) * 127 + 128).toInt()
+            )
+            paint.isAntiAlias = true
+            // Set stroke width based on display density.
+            paint.strokeWidth = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    penInfo.pressure * 10 + 5,
+                    resources.displayMetrics
             )
             paint.style = Paint.Style.STROKE
             paint.strokeJoin = Paint.Join.ROUND
