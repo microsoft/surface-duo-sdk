f4ee2a1ecce0aa3c08c2621c8ef9b6d99fb0111c
Cesar Valiente <cevalien@microsoft.com>
2022-02-07 15:39:42 +0100

    Added new functionality taken/adapted from our compose testing lib and moved from layouts module and adapted to be more generic

--- layouts/layouts/src/androidTest/java/com/microsoft/device/dualscreen/layouts/utils/TestUtils.kt
+++ layouts/layouts/src/androidTest/java/com/microsoft/device/dualscreen/layouts/utils/TestUtils.kt
@@ -5,48 +5,18 @@
 
 package com.microsoft.device.dualscreen.layouts.utils
 
-import android.content.res.Configuration
 import android.view.View
 import androidx.test.espresso.UiController
 import androidx.test.espresso.ViewAction
 import androidx.test.espresso.matcher.BoundedMatcher
 import androidx.test.espresso.matcher.ViewMatchers
 import com.microsoft.device.dualscreen.layouts.FoldableFrameLayout
-import com.microsoft.device.dualscreen.utils.test.DUAL_SCREEN_WIDTH
-import com.microsoft.device.dualscreen.utils.test.HINGE_WIDTH
-import com.microsoft.device.dualscreen.utils.test.SINGLE_SCREEN_WIDTH
+import com.microsoft.device.dualscreen.testing.SurfaceDuo1_dimens
+import com.microsoft.device.dualscreen.testing.areCoordinatesOnTargetScreen
 import com.microsoft.device.dualscreen.utils.wm.DisplayPosition
 import org.hamcrest.Description
 import org.hamcrest.Matcher
-import org.hamcrest.TypeSafeMatcher
 
-fun isViewOnScreen(pos: DisplayPosition, orientation: Int): Matcher<View> =
-    object : TypeSafeMatcher<View>() {
-        override fun describeTo(description: Description?) {
-            description?.appendText(
-                "Checks whether the view is displayed on the right screen"
-            )
-        }
-
-        override fun matchesSafely(item: View?): Boolean {
-            if (item == null) {
-                return false
-            }
-            val startArray = IntArray(2)
-            item.getLocationInWindow(startArray)
-            val start = if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                startArray[0]
-            } else {
-                startArray[1]
-            }
-            val end = if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                startArray[0] + item.width
-            } else {
-                startArray[1] + item.height
-            }
-            return areCoordinatesOnTargetScreen(pos, start, end)
-        }
-    }
 
 fun changeDisplayPosition(pos: DisplayPosition): ViewAction =
     object : ViewAction {
@@ -84,27 +54,12 @@ fun isFrameLayoutOnScreen(pos: DisplayPosition): Matcher<View> =
             val startArray = IntArray(2)
             child.getLocationInWindow(startArray)
             return areCoordinatesOnTargetScreen(
-                pos,
-                startArray[0],
-                startArray[0] + child.width
+                targetScreenPosition = pos,
+                xStart = startArray[0],
+                xEnd = startArray[0] + child.width,
+                firstDisplayWith = SurfaceDuo1_dimens.SINGLE_SCREEN_WIDTH,
+                totalDisplayWith = SurfaceDuo1_dimens.DUAL_SCREEN_WIDTH,
+                foldingFeatureWidth = SurfaceDuo1_dimens.HINGE_WIDTH
             )
         }
     }
-
-fun areCoordinatesOnTargetScreen(
-    targetScreenPosition: DisplayPosition,
-    xStart: Int,
-    xEnd: Int
-): Boolean {
-    return when (targetScreenPosition) {
-        DisplayPosition.DUAL ->
-            xStart in 0..SINGLE_SCREEN_WIDTH &&
-                xEnd in (SINGLE_SCREEN_WIDTH + HINGE_WIDTH)..DUAL_SCREEN_WIDTH
-        DisplayPosition.START ->
-            xStart in 0..SINGLE_SCREEN_WIDTH &&
-                xEnd in 0..SINGLE_SCREEN_WIDTH
-        DisplayPosition.END ->
-            xStart in (SINGLE_SCREEN_WIDTH + HINGE_WIDTH)..DUAL_SCREEN_WIDTH &&
-                xEnd in (SINGLE_SCREEN_WIDTH + HINGE_WIDTH)..DUAL_SCREEN_WIDTH
-    }
-}
---
+++ utils/test-utils/src/main/java/com/microsoft/device/dualscreen/testing/ViewMatcher.kt
@@ -0,0 +1,92 @@
+package com.microsoft.device.dualscreen.testing
+
+import android.content.res.Configuration
+import android.view.View
+import com.microsoft.device.dualscreen.utils.wm.DisplayPosition
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.TypeSafeMatcher
+
+/**
+ * Checks if view given its position {@see DisplayPosition} is on the display given its dimensions.
+ *
+ * @param position: target position.
+ * @param orientation: orientation of the display. See Configuration {@see Configuration}.
+ * @param firstDisplayWith : width of the left/start/first display area
+ * @param totalDisplayWith : width of the right/end/second/total display area
+ * @param foldingFeatureWidth : width of the FoldingFeature {@see FoldingFeature} if any.
+ * @return
+ */
+fun isViewOnScreen(
+    position: DisplayPosition,
+    orientation: Int,
+    firstDisplayWith: Int,
+    totalDisplayWith: Int,
+    foldingFeatureWidth: Int = 0
+): Matcher<View> =
+    object : TypeSafeMatcher<View>() {
+        override fun describeTo(description: Description?) {
+            description?.appendText(
+                "Checks whether the view is displayed on the right screen"
+            )
+        }
+
+        override fun matchesSafely(item: View?): Boolean {
+            if (item == null) {
+                return false
+            }
+            val startArray = IntArray(2)
+            item.getLocationInWindow(startArray)
+            val start = if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                startArray[0]
+            } else {
+                startArray[1]
+            }
+            val end = if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                startArray[0] + item.width
+            } else {
+                startArray[1] + item.height
+            }
+            return areCoordinatesOnTargetScreen(
+                targetScreenPosition = position,
+                xStart = start,
+                xEnd = end,
+                firstDisplayWith = firstDisplayWith,
+                totalDisplayWith = totalDisplayWith,
+                foldingFeatureWidth = foldingFeatureWidth
+            )
+        }
+    }
+
+/**
+ * Checks in the given coordinates {@see targetScreenPosition} are on target screen defined by
+ * {@see xStart}, {@see xEnd}, {@see firstDisplayWith}, {@see totalDisplayWith} and {@see foldingFeatureWidth}.
+ *
+ * @param targetScreenPosition : the target position you want to check.
+ * @param xStart : the start of the screen in the x-axis
+ * @param xEnd : the end of the screen in the y-axis
+ * @param firstDisplayWith : the width of the first display.
+ * @param totalDisplayWith : the width of the total display area.
+ * @param foldingFeatureWidth : the width of the folding feature (if any).
+ * @return
+ */
+fun areCoordinatesOnTargetScreen(
+    targetScreenPosition: DisplayPosition,
+    xStart: Int,
+    xEnd: Int,
+    firstDisplayWith: Int,
+    totalDisplayWith: Int,
+    foldingFeatureWidth: Int = 0
+): Boolean {
+    return when (targetScreenPosition) {
+        DisplayPosition.DUAL ->
+            xStart in 0..firstDisplayWith &&
+                    xEnd in (firstDisplayWith + foldingFeatureWidth)..totalDisplayWith
+        DisplayPosition.START ->
+            xStart in 0..firstDisplayWith &&
+                    xEnd in 0..firstDisplayWith
+        DisplayPosition.END ->
+            xStart in (firstDisplayWith + foldingFeatureWidth)..totalDisplayWith &&
+                    xEnd in (firstDisplayWith + foldingFeatureWidth)..totalDisplayWith
+    }
+}
